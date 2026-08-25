d43190b9a42113bcfaf0ace88b0a26261aba4804
gegj <ulifuqiang@gmail.com>
2026-08-25 09:25:07 +0800

    合并上传菜单，刷新移到最上面

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.23.14.0")]
-[assembly: AssemblyFileVersion("0.23.14.0")]
-[assembly: AssemblyInformationalVersion("0.23.14")]
+[assembly: AssemblyVersion("0.23.15.0")]
+[assembly: AssemblyFileVersion("0.23.15.0")]
+[assembly: AssemblyInformationalVersion("0.23.15")]
