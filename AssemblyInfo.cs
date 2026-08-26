80162c8b63c4e2b5b7c422efc25753f261645544
gegj <ulifuqiang@gmail.com>
2026-08-26 20:47:32 +0800

    修正版本号

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.32.1.0")]
-[assembly: AssemblyFileVersion("0.32.1.0")]
-[assembly: AssemblyInformationalVersion("0.32.1")]
+[assembly: AssemblyVersion("0.32.3.0")]
+[assembly: AssemblyFileVersion("0.32.3.0")]
+[assembly: AssemblyInformationalVersion("0.32.3")]
