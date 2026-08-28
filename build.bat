c15e57b8960ae09a246e5c7fafeea04162b75172
gegj <ulifuqiang@gmail.com>
2026-08-29 00:41:34 +0800

    buils.bat优化

--- build.bat
+++ build.bat
@@ -2,8 +2,8 @@
 chcp 936 >nul
 setlocal
 set "ROOT=%~dp0"
-set "MSBUILD=%ProgramFiles(x86)%\Microsoft Visual Studio\2022\BuildTools\MSBuild\Current\Bin\MSBuild.exe"
-if not exist "%MSBUILD%" set "MSBUILD=%ProgramFiles%\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe"
+set "MSBUILD=%ProgramFiles(x86)%\Microsoft Visual Studio\18\BuildTools\MSBuild\Current\Bin\MSBuild.exe"
+if not exist "%MSBUILD%" set "MSBUILD=%ProgramFiles%\Microsoft Visual Studio\18\BuildTools\MSBuild\Current\Bin\MSBuild.exe"
 if not exist "%MSBUILD%" set "MSBUILD=%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe"
 if not exist "%MSBUILD%" exit /b 1
 "%MSBUILD%" "%ROOT%WiFitool.csproj" /t:Rebuild /p:Configuration=Release
