bbcfbf588b948a6205d9cbcebf5fe3e5e0a291b9
gegj <ulifuqiang@gmail.com>
2026-08-31 14:39:20 +0800

    版本跟新

--- GeneratedAssemblyInfo.cs
+++ GeneratedAssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
-[assembly: AssemblyVersion("0.37.0.0")]
-[assembly: AssemblyFileVersion("0.37.0.0")]
-[assembly: AssemblyInformationalVersion("0.37.0")]
+[assembly: AssemblyVersion("0.38.0.0")]
+[assembly: AssemblyFileVersion("0.38.0.0")]
+[assembly: AssemblyInformationalVersion("0.38.0")]
