500327734eb2243593faff52475ed59ddc33e83f
gegj <ulifuqiang@gmail.com>
2026-09-11 10:03:06 +0800

    刷个版本号

--- GeneratedAssemblyInfo.cs
+++ GeneratedAssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
-[assembly: AssemblyVersion("0.45.3.0")]
-[assembly: AssemblyFileVersion("0.45.3.0")]
-[assembly: AssemblyInformationalVersion("0.45.3")]
+[assembly: AssemblyVersion("0.45.4.0")]
+[assembly: AssemblyFileVersion("0.45.4.0")]
+[assembly: AssemblyInformationalVersion("0.45.4")]
